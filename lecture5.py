454fb71d994ced6c85cb7e40a6d7e8c844f8c5a3
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 22:46:03 +0600

    week1_lecture5

---
+++ lecture5.py
@@ -0,0 +1,29 @@
+# name = "I'm"
+
+# name = input ("Enter your name")
+# # surname = input("Enter your surname")
+# # full_name = (name + ' ' +surname)
+# # print(full_name)
+
+# times = int(input("How many times you want to print you name: "))
+
+# result = times * name
+# print(result)
+
+# word = "abcdefghklmn"
+# print(word[3])
+# # word[x:y:z]    # beret vse s konsa -1
+# new_word = (word[1:10:2])
+# print(new_word)
+# word = "Adelinamaster degree\a"
+# print(word)
+# word = "\172"
+# print(word)           #READ UNICODE ASCII
+
+# word = "Adelina\\masterdegree"
+# print(word)
+
+# word = '''Adelina
+# master degree'''        #Mmogostrochniye stroki
+# print(word)
+
